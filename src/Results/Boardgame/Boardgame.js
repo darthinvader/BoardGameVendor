74bfca69542845401d87aa7e786b2f55bfbade12
darthinvader <psaradakisj@gmail.com>
2021-07-27 11:26:41 +0100

    [feat] Boardgame css and js

---
+++ src/Results/Boardgame/Boardgame.js
@@ -0,0 +1,37 @@
+import { BsClock, BsFillHeartFill } from "react-icons/bs";
+import { GiAges } from "react-icons/gi";
+import { IoIosPeople } from "react-icons/io";
+import styles from "./Boardgame.module.scss";
+
+const Boardgame = ({ game }) => {
+  const description = game.description;
+  const thumbnail = game.thumbnail;
+  const maxPlayers = game.maxPlayers;
+  const minPlayers = game.minPlayers;
+  const minAge = game.minAge;
+  const minPlaytime = game.minPlaytime;
+  const maxPlaytime = game.maxPlaytime;
+  const title = game.name;
+
+  <div className={styles.Boardgame}>
+    <img alt={title} src={thumbnail} className={styles.Image} />
+    <h2 className={styles.Title}>{title}</h2>
+    <p className={styles.Description}>{description}</p>
+    <div className={styles.Information}>
+      <div>
+        <IoIosPeople /> {minPlayers}-{maxPlayers}
+      </div>
+      <div>
+        <BsClock /> {minPlaytime}-{maxPlaytime}
+      </div>
+      <div>
+        <GiAges /> {minAge}+
+      </div>
+      <div>
+        <BsFillHeartFill />
+      </div>
+    </div>
+  </div>;
+};
+
+export default Boardgame;
