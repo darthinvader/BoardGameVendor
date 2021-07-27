3bef5ccbdb6f027cc7fa1ccb8a518aed6b76cbb4
darthinvader <psaradakisj@gmail.com>
2021-07-27 12:41:29 +0100

    [feat] dynamic description size

--- src/Results/Boardgame/Boardgame.js
+++ src/Results/Boardgame/Boardgame.js
@@ -2,8 +2,11 @@ import { BsClock, BsFillHeartFill } from "react-icons/bs";
 import { GiAges } from "react-icons/gi";
 import { IoIosPeople } from "react-icons/io";
 import styles from "./Boardgame.module.scss";
+import useWindowDimensions from "./useWindowDimensions";
 
 const Boardgame = ({ game }) => {
+  const { width, _ } = useWindowDimensions();
+
   const description = game.description;
   const thumbnail = game.thumbnail;
   const maxPlayers = game.maxPlayers;
@@ -12,12 +15,19 @@ const Boardgame = ({ game }) => {
   const minPlaytime = game.minPlaytime;
   const maxPlaytime = game.maxPlaytime;
   const title = game.name;
-
+  let subDescription = "";
+  if (width >= 550) {
+    subDescription = description.substring(0, 500) + "...";
+  } else if (width < 400) {
+    subDescription = description.substring(0, 200) + "...";
+  } else if (width < 550) {
+    subDescription = description.substring(0, 300) + "...";
+  }
   return (
-    <div className={styles.Boardgame}>
+    <div className={styles.BoardGame}>
       <img alt={title} src={thumbnail} className={styles.Image} />
       <h2 className={styles.Title}>{title}</h2>
-      <p className={styles.Description}>{description}</p>
+      <p className={styles.Description}>{subDescription}</p>
       <div className={styles.Information}>
         <div>
           <IoIosPeople /> {minPlayers}-{maxPlayers}
@@ -28,7 +38,7 @@ const Boardgame = ({ game }) => {
         <div>
           <GiAges /> {minAge}+
         </div>
-        <div>
+        <div className={styles.Favorite}>
           <BsFillHeartFill />
         </div>
       </div>
