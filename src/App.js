32440f9cb39f5727cb3b9d92807926dc396b2dc3
darthinvader <psaradakisj@gmail.com>
2021-07-28 14:10:44 +0100

    [feat] Added Navbar

--- src/App.js
+++ src/App.js
@@ -2,15 +2,29 @@ import SearchContainer from "./SearchContainer/SearchContainer";
 import { useState } from "react";
 import FavoritesContainer from "./FavoritesContainer/FavoritesContainer";
 import styles from "./App.module.scss";
-const SEARCH = "SEARCH";
-const FAVORITES = "FAVORITES";
+const SEARCH = "search";
+const FAVORITES = "favorites";
 
 function App() {
   const [selectedTab, setSelectedTab] = useState(SEARCH);
 
   const tab =
     selectedTab === SEARCH ? <SearchContainer /> : <FavoritesContainer />;
-  return <div className={styles.Container}>{tab}</div>;
+
+  const SearchActive = selectedTab === SEARCH ? styles.NavElementActive : "";
+  const FavoritesActive =
+    selectedTab === FAVORITES ? styles.NavElementActive : "";
+  return (
+    <div className={styles.Container}>
+      <nav className={styles.Navbar}>
+        <div className={styles.NavElement + " " + SearchActive}>Search</div>
+        <div className={styles.NavElement + " " + FavoritesActive}>
+          Favorites
+        </div>
+      </nav>
+      {tab}
+    </div>
+  );
 }
 
 export default App;
