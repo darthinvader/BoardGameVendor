1b7e1a8baca4b5e803a8d434dab76eed2fbd9c25
darthinvader <psaradakisj@gmail.com>
2021-07-30 18:28:23 +0100

    [feat] Context custom hooks

--- src/Context/DBContext.js
+++ src/Context/DBContext.js
@@ -1,11 +1,31 @@
-import { useState, useEffect } from "react";
+import axios from "axios";
+import React, { useState, useEffect } from "react";
+
+export const useDB = () => DBContext;
+export const useDBUpdatae = () => DBUpdateContext;
 
 const DBContext = React.createContext("loading");
+const DBUpdateContext = React.createContext();
 
 const DBProvider = ({ children }) => {
   const [DBGames, setDBGames] = useState("loading");
 
-  return <DBContext.Provider value={DBGames}>{children}</DBContext.Provider>;
+  useEffect(() => {
+    updateDBGames();
+  }, []);
+
+  const updateDBGames = async () => {
+    const games = axios.get("http://localhost:8080/games");
+    setDBGames(await games.data);
+  };
+
+  return (
+    <DBContext.Provider value={DBGames}>
+      <DBUpdateContext.Provider value={updateDBGames}>
+        {children}
+      </DBUpdateContext.Provider>
+    </DBContext.Provider>
+  );
 };
 
 export default DBProvider;
