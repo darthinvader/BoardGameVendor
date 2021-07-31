3c50bfc8df0b2c7493ff593569d48f25a37d79fd
darthinvader <psaradakisj@gmail.com>
2021-07-31 16:49:15 +0100

    [feat] used useCallback

--- src/Context/DBContext.js
+++ src/Context/DBContext.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useCallback } from "react";
 
 export const useDB = () => DBContext;
 export const useDBUpdatae = () => DBUpdateContext;
@@ -10,34 +10,44 @@ const DBUpdateContext = React.createContext();
 const DBProvider = ({ children }) => {
   const [DBGames, setDBGames] = useState("loading");
 
-  useEffect(() => {
-    getDBGames();
-  }, []);
-
-  const getDBGames = async () => {
+  const getDBGames = useCallback(async () => {
     const games = await axios.get("http://localhost:8080/games");
     setDBGames(await games.data);
-  };
+  }, []);
 
-  const addGameToDB = (game) => {
-    const newGame = { ...game };
-    newGame.description = "";
-    axios.post(`http://localhost:8080/games`, newGame).then(() => {
-      getDBGames();
-    });
-    console.log(game);
-  };
+  const addGameToDB = useCallback(
+    (game) => {
+      const newGame = { ...game };
+      newGame.description = "";
+      axios.post(`http://localhost:8080/games`, newGame).then(() => {
+        getDBGames();
+      });
+    },
+    [getDBGames]
+  );
 
-  const updateGame = (game, description) => {
-    const id = game.id;
-    const update = { description };
+  const updateGame = useCallback(
+    (game, description) => {
+      const id = game.id;
+      const update = { description };
 
-    axios.patch(`http://localhost:8080/games/${id}`, update).then(getDBGames());
-  };
+      axios
+        .patch(`http://localhost:8080/games/${id}`, update)
+        .then(getDBGames());
+    },
+    [getDBGames]
+  );
 
-  const deleteGame = (id) => {
-    axios.delete(`http://localhost:8080/games/${id}`).then(getDBGames());
-  };
+  const deleteGame = useCallback(
+    (id) => {
+      axios.delete(`http://localhost:8080/games/${id}`).then(getDBGames());
+    },
+    [getDBGames]
+  );
+
+  useEffect(() => {
+    getDBGames();
+  }, [getDBGames]);
 
   return (
     <DBContext.Provider value={DBGames}>
