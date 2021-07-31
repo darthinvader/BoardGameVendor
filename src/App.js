164e138b0d8ad46e5e1ef6b283bab5178b1aa300
darthinvader <psaradakisj@gmail.com>
2021-07-31 16:08:37 +0100

    [feat] added DBProvider to App

--- src/App.js
+++ src/App.js
@@ -3,22 +3,25 @@ import FavoritesContainer from "./FavoritesContainer/FavoritesContainer";
 import styles from "./App.module.scss";
 import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
 import Navbar from "./Navbar/Navbar";
+import DBProvider from "./Context/DBContext";
 
 function App() {
   return (
-    <div className={styles.Container}>
-      <Router>
-        <Navbar />
-        <Switch>
-          <Route exact path="/">
-            <SearchContainer />
-          </Route>
-          <Route exact path="/Favorites">
-            <FavoritesContainer />
-          </Route>
-        </Switch>
-      </Router>
-    </div>
+    <DBProvider>
+      <div className={styles.Container}>
+        <Router>
+          <Navbar />
+          <Switch>
+            <Route exact path="/">
+              <SearchContainer />
+            </Route>
+            <Route exact path="/Favorites">
+              <FavoritesContainer />
+            </Route>
+          </Switch>
+        </Router>
+      </div>
+    </DBProvider>
   );
 }
 
