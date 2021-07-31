36b12232171d76a4fd6a3606095a1da30e0b2da2
darthinvader <psaradakisj@gmail.com>
2021-07-31 16:08:22 +0100

    [feat] added CRUD operation

--- src/Context/DBContext.js
+++ src/Context/DBContext.js
@@ -19,9 +19,31 @@ const DBProvider = ({ children }) => {
     setDBGames(await games.data);
   };
 
+  const addGameToDB = (game) => {
+    const newGame = { ...game };
+    newGame.description = "";
+    axios.post(`http://localhost:8080/games`, newGame).then(() => {
+      getDBGames();
+    });
+    console.log(game);
+  };
+
+  const updateGame = (game, description) => {
+    const id = game.id;
+    const update = { description };
+
+    axios.patch(`http://localhost:8080/games/${id}`, update).then(getDBGames());
+  };
+
+  const deleteGame = (id) => {
+    axios.delete(`http://localhost:8080/games/${id}`).then(getDBGames());
+  };
+
   return (
     <DBContext.Provider value={DBGames}>
-      <DBUpdateContext.Provider value={getDBGames}>
+      <DBUpdateContext.Provider
+        value={{ getDBGames, addGameToDB, updateGame, deleteGame }}
+      >
         {children}
       </DBUpdateContext.Provider>
     </DBContext.Provider>
