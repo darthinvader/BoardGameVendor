33739dd71c9e06b02f6ae00f33cef4d686442ddc
darthinvader <psaradakisj@gmail.com>
2021-08-02 17:19:28 +0100

    [feat] getGames and map them

--- src/FavoritesContainer/FavoritesContainer.js
+++ src/FavoritesContainer/FavoritesContainer.js
@@ -1,9 +1,18 @@
 import { useDB } from "../Context/DBContext";
+import FavoriteGame from "./FavoriteGame/FavoriteGame";
 
 const FavoritesContainer = () => {
   const games = useDB();
+  console.log(games);
+  if (games === "loading") {
+    return <p>Loading...</p>;
+  }
 
-  return <p>Loading...</p>;
+  const boardGames = games.map((game, i) => (
+    <FavoriteGame game={game} key={game.title + i.toString()} />
+  ));
+
+  return null;
 };
 
 export default FavoritesContainer;
